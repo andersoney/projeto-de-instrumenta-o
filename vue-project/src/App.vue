0d8e42dd71ad2c0b1aa155db8a4ba7b85190c724
Andersoney Rodrigues <andersoney@gmail.com>
2022-02-15 22:09:14 -0300

    feat: front com vue-chart

---
+++ vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="chart">
+        <TheWelcome />
+      </div>
+      <div class="chart">
+        <TheWelcome />
+      </div>
+    </main>
+  </div>
+</template>
+<script>
+import TheWelcome from "./components/TheWelcome.vue";
+export default {
+  components: { TheWelcome },
+};
+</script>
+<style>
+.chart {
+  width: 40%;
+  position: relative;
+}
+</style>
+
---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,41 @@
+<script>
+import { Line } from "vue-chartjs";
+export default {
+  name: "LineChart",
+  extends: Line,
+  data: () => ({
+    chartdata: {
+      labels: ["1", "2", "3", "4", "5", "6", "7"],
+      datasets: [
+        {
+          label: "Data One",
+          backgroundColor: "#f87979",
+          data: [40, 39, 10, 40],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        duration: 0,
+      },
+    },
+    number: 0,
+  }),
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+    setInterval(() => {
+      let vetor = [];
+      this.chartdata.datasets[0].data.push(Math.random() * 90 + 10);
+      // this.chartdata.datasets[0].data.push(this.number);
+      this.chartdata.labels.push(this.number);
+      if (this.chartdata.datasets[0].data.length > 100) {
+        this.chartdata.datasets[0].data.splice();
+      }
+      this.renderChart(this.chartdata, this.options);
+      this.number++;
+    }, 1000);
+  },
+};
+</script>
